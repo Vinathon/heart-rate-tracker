789c5fd629ca46736975c343fda17d94b332569c
Vinathon <vinishna@gmail.com>
2024-09-28 11:52:18 +0530

    updated config file

--- config/config.js
+++ config/config.js
@@ -1,15 +1,20 @@
 // config/config.js
 require('dotenv').config();
-
+const fs = require('fs');
 module.exports = {
-  port: process.env.PORT || 3000,
-  intervalDurationMinutes: Number(process.env.INTERVAL_DURATION_MINUTES) || 15,
-  payloadSizeLimit: process.env.PAYLOAD_SIZE_LIMIT || '5mb',
-//   db: {
-//     user: process.env.DB_USER ,
-//     host: process.env.DB_HOST || 'localhost',
-//     database: process.env.DB_NAME ,
-//     password: process.env.DB_PASSWORD,
-//     port: Number(process.env.DB_PORT) || 5432,
-//   },
+    port: process.env.PORT || 3000,
+    intervalDurationMinutes: Number(process.env.INTERVAL_DURATION_MINUTES) || 15,
+    payloadSizeLimit: process.env.PAYLOAD_SIZE_LIMIT || '5mb',
+    db: {
+        user: process.env.DB_USER ,
+        password: process.env.DB_PASSWORD ,
+        host: process.env.DB_HOST ,
+        port: Number(process.env.DB_PORT) ,
+        database: process.env.DB_NAME ,
+        ssl: {
+            rejectUnauthorized: true,
+            ca: fs.readFileSync(process.env.DB_SSL_CERT_PATH).toString(),
+        },
+    }
+
 };
