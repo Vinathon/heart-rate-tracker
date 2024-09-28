b906566465cc6f0e2f6c44dc0c0b4124862ebc43
Vinathon <vinishna@gmail.com>
2024-09-28 11:52:02 +0530

    retreve data based on patientId

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const clinicalDataController = require('../controllers/clinicalDataController');
+const validatePayload = require('../middleware/validationMiddleware');
 
-router.post('/', clinicalDataController.receiveClinicalData);
+router.post('/', validatePayload,clinicalDataController.receiveClinicalData);
+
+router.get('/:patientId', clinicalDataController.getClinicalData);
 
 module.exports = router;
